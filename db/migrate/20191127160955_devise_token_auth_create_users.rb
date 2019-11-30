1ceec3b6b07cecb07acba08d5aeb08524977b47f
akidai <akidaitunes@gmail.com>
2019-11-30 14:30:06 +0900

    Add Banned Column To User Table

--- db/migrate/20191127160955_devise_token_auth_create_users.rb
+++ db/migrate/20191127160955_devise_token_auth_create_users.rb
@@ -38,14 +38,12 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
       t.json :tokens
 
       t.timestamps
-      t.is_editing_banned :boolean
     end
 
     add_index :users, :email,                unique: true
     add_index :users, [:uid, :provider],     unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
-    add_index :users, :is_editing_banned
     # add_index :users, :unlock_token,       unique: true
   end
 end
---
+++ db/migrate/20191130052817_add_banned_to_user.rb
@@ -0,0 +1,5 @@
+class AddBannedToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :banned, :boolean, default: false, null: false
+  end
+end
