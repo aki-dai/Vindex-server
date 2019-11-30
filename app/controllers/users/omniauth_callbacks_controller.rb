633895574b4e80f9fff021fd6a44e920e820d7d4
akidai <akidaitunes@gmail.com>
2019-11-30 14:21:35 +0900

    auth json modify

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,8 +6,26 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   include ActionView::Rendering
   # You should also create an action method in this controller like this:
   def redirect_callbacks
-    auth = request.env['omniauth.auth']
-    render :json => auth
+   auth = request.env['omniauth.auth']
+   user_data = {
+    provider:   auth.provider,
+    uid:        auth.uid,
+    name:       auth.info.name,
+    nickname:   auth.info.nickname,
+    image:      auth.info.image,
+  }
+
+    @resistered_user = User.find_by(uid: auth.uid)
+    if !@resistered_user
+      user = User.new(user_data)
+      user.save!
+    end
+
+    response = {
+      success: true,
+      data: user_data,
+    }
+    render :json => response
   end
 
   def omniauth_failure
@@ -28,10 +46,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
      super
   end
 
-   protected
+   private
 
   #The path used when OmniAuth fails
-   def after_omniauth_failure_path_for(scope)
-     super(scope)
+   def do_callback(omniauth)
+      uid       = omniauth.uid
+      provider  = omniauth.uid
+      name      = omniauth.info.name
+      image_url = omniauth.info.image
    end
 end
