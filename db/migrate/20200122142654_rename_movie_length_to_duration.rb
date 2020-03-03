03be9842a2fa7c6dc9d550dcf43d2f97e11b2c13
akidai <akidaitunes@gmail.com>
2020-03-03 22:50:41 +0900

    complete move to docker!

--- db/migrate/20200122142654_rename_movie_length_to_duration.rb
+++ db/migrate/20200122142654_rename_movie_length_to_duration.rb
@@ -1,6 +1,5 @@
 class RenameMovieLengthToDuration < ActiveRecord::Migration[5.2]
   def change
     rename_column :movies, :movie_length, :duration
-    add_column :movies, :provider, :string
   end
 end
